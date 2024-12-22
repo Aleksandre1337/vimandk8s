870aee9ba1bd1c51f8cb5e74d6bd1c26223841f9
Aleksandre <55573405+Aleksandre1337@users.noreply.github.com>
2024-12-22 16:48:41 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,20 +8,12 @@ echo "Running the script as user: $CURRENT_USER"
 read -p "Are you using yum or apt package manager? " packagemanager
 
 # Install vim and ncurses
-if ! command -v vim &> /dev/null; then
-  sudo $packagemanager install -y vim
-  echo "Vim installed successfully"
-elif ! command -v clear &> /dev/null; then
-  sudo $packagemanager install -y ncurses
-  echo "Ncurses package installed, you can use clear command now"
-else
-  echo "Packages are already installed, moving on"
-fi
+sudo $packagemanager install -y vim ncurses
+echo "Vim and ncurses installed successfully"
 
 # Create .vimrc file with the specified settings
 VIMRC_PATH="/home/$CURRENT_USER/.vimrc"
-if [ ! -f "$VIMRC_PATH" ]; then
-  cat <<EOL > "$VIMRC_PATH"
+cat <<EOL > "$VIMRC_PATH"
 set ts=2
 set sw=2
 set et
@@ -29,31 +21,17 @@ set ai
 set si
 set ic
 EOL
-  echo ".vimrc created"
-else
-  echo ".vimrc already exists, moving on"
-fi
+echo ".vimrc updated with necessary settings"
 
-# Update .bashrc with the specified environment variables and alias
+# Update .bashrc with the specified environment variables
 BASHRC_PATH="/home/$CURRENT_USER/.bashrc"
-if ! grep -q "export do=" "$BASHRC_PATH"; then
-  cat <<EOL >> "$BASHRC_PATH"
+cat <<EOL >> "$BASHRC_PATH"
 export do="--dry-run=client -o yaml"
 export now="--force --grace-period 0"
 EOL
-  echo "Environment variables added to .bashrc"
-else
-  echo "Environment variables already set in .bashrc, moving on"
-fi
-
-if ! grep -q "alias k=" "$BASHRC_PATH"; then
-  echo 'alias k=/usr/local/bin/kubectl' >> "$BASHRC_PATH"
-  echo "Alias added to .bashrc"
-else
-  echo "Alias already set in .bashrc, moving on"
-fi
+echo "Environment variables added to .bashrc"
 
 # Source the .bashrc to apply changes as the current user
-sudo -u $CURRENT_USER -i bash -c "source $BASHRC_PATH"
+source "$BASHRC_PATH"
 
-echo "Vim installed, .vimrc and .bashrc updated."
+echo "Script Complete"
